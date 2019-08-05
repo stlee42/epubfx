88be185eb87aeeff8a4bd760835ea8ff14de1cb7
Michail Jungierek <michail@jungierek.eu>
2019-08-05 12:32:17 +0200

    tried to implement extended key combinations

--- src/main/java/de/machmireinebook/epubeditor/editor/AbstractRichTextCodeEditor.java
+++ src/main/java/de/machmireinebook/epubeditor/editor/AbstractRichTextCodeEditor.java
@@ -66,11 +66,12 @@ public abstract class AbstractRichTextCodeEditor extends AnchorPane implements C
     private IntegerProperty cursorPosition = new SimpleIntegerProperty();
     private boolean isChangingCode = false;
 
-    // textInformationProperty
     private final ReadOnlyStringWrapper textInformation = new ReadOnlyStringWrapper(this, "textInformation");
     final PreferencesManager preferencesManager = BeanFactory.getInstance().getBean(PreferencesManager.class);
     protected MediaType mediaType;
-    int durationHighlightingComputation = 10;
+    /*package private*/ int durationHighlightingComputation = 10;
+    private boolean extendedKeyInputModeAltGrD;
+    private boolean extendedKeyInputModeAltGrUmlautA;
 
     AbstractRichTextCodeEditor()
     {
@@ -122,11 +123,53 @@ public abstract class AbstractRichTextCodeEditor extends AnchorPane implements C
         CodeArea codeArea = getCodeArea();
         Nodes.removeInputMap(codeArea, consume(keyPressed(KeyCode.TAB), this::insertTab));
         Nodes.addInputMap(codeArea, sequence(
+                                    consume(keyPressed(KeyCode.D, KeyCombination.CONTROL_DOWN, KeyCombination.ALT_DOWN), keyEvent -> {
+                                        logger.info("switch to AltGr-D");
+                                        extendedKeyInputModeAltGrD = true;
+                                        keyEvent.consume();
+                                    }),
+                                    consume(keyPressed(KeyCode.X), keyEvent -> {
+                                        if (extendedKeyInputModeAltGrD) {
+                                            insertAt(getAbsoluteCursorPosition(), "„");
+                                            extendedKeyInputModeAltGrD = false;
+                                        }
+                                    }),
+                                    consume(keyPressed(KeyCode.V), keyEvent -> {
+                                        if (extendedKeyInputModeAltGrD) {
+                                            insertAt(getAbsoluteCursorPosition(), "“");
+                                            extendedKeyInputModeAltGrD = false;
+                                        }
+                                    }),
+                                    consume(keyPressed(KeyCode.B), keyEvent -> {
+                                        if (extendedKeyInputModeAltGrD) {
+                                            insertAt(getAbsoluteCursorPosition(), "”");
+                                            extendedKeyInputModeAltGrD = false;
+                                        }
+                                    }),
+                                    consume(keyPressed(KeyCode.X, KeyCombination.SHIFT_DOWN), keyEvent -> {
+                                        if (extendedKeyInputModeAltGrD) {
+                                            insertAt(getAbsoluteCursorPosition(), "‚");
+                                            extendedKeyInputModeAltGrD = false;
+                                        }
+                                    }),
+                                    consume(keyPressed(KeyCode.V, KeyCombination.SHIFT_DOWN), keyEvent -> {
+                                        if (extendedKeyInputModeAltGrD) {
+                                            insertAt(getAbsoluteCursorPosition(), "‘");
+                                            extendedKeyInputModeAltGrD = false;
+                                        }
+                                    }),
+                                    consume(keyPressed(KeyCode.B, KeyCombination.SHIFT_DOWN), keyEvent -> {
+                                        if (extendedKeyInputModeAltGrD) {
+                                            insertAt(getAbsoluteCursorPosition(), "’");
+                                            extendedKeyInputModeAltGrD = false;
+                                        }
+                                    }),
                                     consume(keyPressed(KeyCode.TAB), this::insertTab),
-                                    consume(keyPressed(KeyCode.TAB, KeyCombination.SHIFT_DOWN), this::shiftTabPressed)));
+                                    consume(keyPressed(KeyCode.TAB, KeyCombination.SHIFT_DOWN), this::insertTab)));
 
         codeArea.setOnKeyTyped(e -> {
             String character = e.getCharacter();
+            logger.info("typed character " + character);
             switch (character) {
                 case "{":
                     completePair("}");
@@ -142,6 +185,7 @@ public abstract class AbstractRichTextCodeEditor extends AnchorPane implements C
                     break;
             }
         });
+
         codeArea.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> {
             if (event.getClickCount() == 1 && event.isSecondaryButtonDown()) {
                 CharacterHit hit = codeArea.hit(event.getX(), event.getY());
